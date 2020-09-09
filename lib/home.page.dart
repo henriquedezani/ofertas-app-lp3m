0fe2bd88b8d2d23e092c1f771120751b217453ac
Henrique Dezani <dezani@gmail.com>
2020-09-09 12:52:12 -0300

    Versão da aula 09-09

--- lib/home.page.dart
+++ lib/home.page.dart
@@ -11,8 +11,60 @@ class HomePage extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("Ofertas"),
       ),
-      body: new Center(
-        child: new Text("Hello World"),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(12, 12, 12, 0),
+        padding: EdgeInsets.all(8),
+        //color: Colors.white,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.1),
+                offset: Offset(4, 4),
+                blurRadius: 12,
+              ),
+            ]),
+        child: Row(
+          // mainAxisSize: MainAxisSize.min,
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              'https://via.placeholder.com/100x100.png?text=Produto1',
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Computador Dell Vostro 1400, Intel i9, 16GB RAM e 1TB SSD",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "www.kabum.com.br",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                Icon(Icons.thumb_up, size: 16),
+                Text(
+                  "R\$ 1560,00",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
