68952d7479bd6923f4812fe06f8b00064e08a1df
Henrique Dezani <dezani@gmail.com>
2020-10-07 10:57:54 -0300

    Versão da aula 10-07

--- lib/detalhe.page.dart
+++ lib/detalhe.page.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class DetalhePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Image.network(
-        'https://via.placeholder.com/100x100.png?text=Produto1',
+      appBar: AppBar(
+        title: Text("Oferta"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+                "https://via.placeholder.com/100x100.png?text=Produto1",
+                fit: BoxFit.cover),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Computador Intel i9",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "www.kabum.com.br",
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey,
+                  ),
+                ),
+                SmoothStarRating(
+                  rating: 3.5,
+                  size: 20,
+                  isReadOnly: false,
+                  filledIconData: Icons.star,
+                  halfFilledIconData: Icons.star_half,
+                  defaultIconData: Icons.star_border,
+                  starCount: 5,
+                  allowHalfRating: true,
+                  spacing: 2.0,
+                  color: Colors.amber,
+                  borderColor: Colors.amber,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "R\$ 1.560,00",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  width: double.infinity,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Ir para a oferta",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
